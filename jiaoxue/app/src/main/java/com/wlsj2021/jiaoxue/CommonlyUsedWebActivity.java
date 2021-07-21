9fff630fdbe433a706b427490d7b3aaebf5bbc70
wlsj2021 <87267648+wlsj2021@users.noreply.github.com>
2021-07-21 16:21:21 +0800

    动画
    补间 帧 属性 动画

--- jiaoxue/app/src/main/java/com/wlsj2021/jiaoxue/CommonlyUsedWebActivity.java
+++ jiaoxue/app/src/main/java/com/wlsj2021/jiaoxue/CommonlyUsedWebActivity.java
@@ -3,8 +3,23 @@ package com.wlsj2021.jiaoxue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import com.tbruyelle.rxpermissions.RxPermissions;
 import com.trello.rxlifecycle.ActivityEvent;
@@ -18,6 +33,8 @@ import java.security.acl.Permission;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -39,10 +56,133 @@ import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class CommonlyUsedWebActivity extends RxAppCompatActivity {
+
+    @BindView(R.id.imageViewAnim)
+    ImageView mImageView;
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commonly_used_web);
+        ButterKnife.bind(this);
+
+//帧动画
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_background),100);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_foreground),100);
+//        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_background),100);
+//        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_background),100);
+//        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_background),100);
+//        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_background),100);
+//        animationDrawable.addFrame(getResources().getDrawable(R.drawable.ic_launcher_background),100);
+        animationDrawable.setOneShot(false);
+        mImageView.setImageDrawable(animationDrawable);
+//        animationDrawable.start();
+
+//补间动画 透明 位移 旋转 缩放
+        //补间动画透明度
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1,0);
+        alphaAnimation.setRepeatCount(10);
+        alphaAnimation.setDuration(1000);
+//        mImageView.setAnimation(alphaAnimation);
+        //差值器 改变动画的运动状态
+        //估值器 改变动画的运动值
+        alphaAnimation.setInterpolator(new BounceInterpolator());
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Log.e("TAG", "onAnimationStart: ",null );
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Log.e("TAG", "onAnimationEnd: ", null);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                Log.e("TAG", "onAnimationRepeat: ", null);
+
+            }
+        });
+//        alphaAnimation.start();
+        //补间动画平移
+
+        TranslateAnimation translateAnimation = new TranslateAnimation(1,100,3,100);
+        translateAnimation.setDuration(2000);
+        translateAnimation.setRepeatCount(5);
+        translateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+//        mImageView.setAnimation(translateAnimation);
+//        translateAnimation.start();
+//        mImageView.setVisibility(View.INVISIBLE);
+        //补间动画旋转
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0,180,Animation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setDuration(2000);
+        rotateAnimation.setRepeatCount(5);
+        rotateAnimation.setInterpolator(new BounceInterpolator());
+//        mImageView.setAnimation(rotateAnimation);
+//        rotateAnimation.start();
+
+        //补间动画缩放
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1,10,3,10,Animation.ABSOLUTE,0.5f,Animation.ABSOLUTE,0.5f);
+        scaleAnimation.setDuration(2000);
+        scaleAnimation.setRepeatCount(5);
+        scaleAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+//        mImageView.setAnimation(scaleAnimation);
+//        scaleAnimation.start();
+
+
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.setDuration(5000);
+        //补间动画透明度
+        animationSet.addAnimation(alphaAnimation);
+        //补间动画缩放
+        animationSet.addAnimation(scaleAnimation);
+        //补间动画平移
+        animationSet.addAnimation(translateAnimation);
+        //补间动画旋转
+        animationSet.addAnimation(rotateAnimation);
+        animationSet.setInterpolator(new BounceInterpolator());
+//        mImageView.setAnimation(animationSet);
+//        animationSet.start();
+//ARGB
+        //Alpha Red Green Blue  ARGB8888 ARGB4444  RGB565
+//属性动画 valueAnimator
+        //透明
+        ObjectAnimator alphaObjectAnimator = ObjectAnimator.ofFloat(mImageView,"alpha",1f,0f,1f,0f,0.5f,1f,0.7f,0.3f);
+        alphaObjectAnimator.setInterpolator(new BounceInterpolator());
+        alphaObjectAnimator.setDuration(10000);
+//        alphaObjectAnimator.start();
+        //缩放
+        ObjectAnimator scaleObjectAnimator = ObjectAnimator.ofFloat(mImageView,"scaleY",1f,3f,6f,2f,1f,6f);
+        scaleObjectAnimator.setDuration(10000);
+//        scaleObjectAnimator.start();
+        //平移
+        ObjectAnimator translationObjectAnimator = ObjectAnimator.ofFloat(mImageView,"translationY",0,100,0,-100,50.0f,-100);
+        translationObjectAnimator.setInterpolator(new BounceInterpolator());
+        translationObjectAnimator.setDuration(10000);
+//        translationObjectAnimator.start();
+        //旋转
+        ObjectAnimator rotationObjectAnimator = ObjectAnimator.ofFloat(mImageView,"rotation",0,150,-90,360,180,-360);
+        rotationObjectAnimator.setDuration(10000);
+//        rotationObjectAnimator.start();
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(10000);
+        animatorSet.setInterpolator(new BounceInterpolator());
+//        animatorSet.playTogether(
+//                alphaObjectAnimator,
+//                scaleObjectAnimator,
+//                translationObjectAnimator,
+//                rotationObjectAnimator
+//        );
+        animatorSet.play(alphaObjectAnimator).before(scaleObjectAnimator).after(rotationObjectAnimator).with(translationObjectAnimator);
+        animatorSet.start();
+
+
 
 
         RxPermissions rxPermissions = new RxPermissions(this);
